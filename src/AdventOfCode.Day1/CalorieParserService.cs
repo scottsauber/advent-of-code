2e715be709703ee1eb2ae264006aba2ed1470c39
Scott Sauber <stsauber@gmail.com>
2022-12-23 20:46:59 -0600

    hook it up

--- src/AdventOfCode.Day1/CalorieParserService.cs
+++ src/AdventOfCode.Day1/CalorieParserService.cs
@@ -4,13 +4,14 @@ public class CalorieParserService
 {
     public int FindElfWithMostCalories(string elvesCarryingCalories)
     {
-        return elvesCarryingCalories.Split(Environment.NewLine + Environment.NewLine)
+        return elvesCarryingCalories
+            .Split(Environment.NewLine + Environment.NewLine)
             .Select((x, i) => new Elf
             {
                 Number = i + 1,
-                CaloriesBeingCarried = x.Split(Environment.NewLine).Select(x => int.Parse(x)).ToList()
+                CaloriesBeingCarried = x.Split(Environment.NewLine).Select(c => int.Parse(c)).ToList()
             })
-            .MaxBy(x => x.CaloriesBeingCarried.Sum())
+            .MaxBy(x => x.CaloriesBeingCarried.Sum())!
             .Number;
     }
 }
--- src/AdventOfCode.Day1/Program.cs
+++ src/AdventOfCode.Day1/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine();
+﻿using AdventOfCode.Day1;
+
+var file = File.ReadAllText("Input.txt");
+var calorieParserService = new CalorieParserService();
+var elfNumber = calorieParserService.FindElfWithMostCalories(file);
+
+Console.WriteLine(elfNumber);
